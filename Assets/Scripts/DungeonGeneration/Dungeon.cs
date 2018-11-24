b4b8d0a6433acc729b1f1f43af9fe69d70251ff2
Banzindun <banzindun@gmail.com>
2018-11-24 23:46:04 +0100

    ADde more stuff.

---
+++ Assets/Scripts/DungeonGeneration/Dungeon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dungeon {
+
+    public float Difficulty;
+
+    private Queue<GameObject> monsters;
+        
+    public Dungeon()
+    {
+        monsters = new Queue<GameObject>();
+    }
+
+    public bool IsAvailable() {
+        return monsters.Count > 0;
+    }
+
+    public GameObject NextMonster() {
+        return monsters.Dequeue();
+    }
+
+    public void AddMonster(GameObject monster) {
+        monsters.Enqueue(monster);
+    }
+
+
+
+}
---
+++ Assets/Scripts/DungeonGeneration/DungeonConstants.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GenerationConstants.asset", menuName = "Generation / DungeonConstants")]
+public class DungeonConstants : ScriptableObject {
+
+    public int NumberOfMonsters;
+
+    // Modifies time
+    public float DifficultyModifier;
+
+    public Colorable[] Colorables;
+
+}
