2bf01d9e931ba6cfb63ef758fb4ce28720fbc93d
Jakub <jakub.svoboda8495@gmail.com>
2018-11-24 18:29:22 +0100

    Added timer, cannot change colors when timer runs out

---
+++ Assets/Scripts/Coloring/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	public Timer coloringTimer = null;
+	public float CurrentDifficultyModifier = 1f;
+	public bool IsDoingColoring = false;
+	public ColorWheel colorWheelPrefab = null;
+	private bool alreadySetupBeforeColoring = false;
+	private Colorable currentColorable = null;
+	private ColorWheel colorWheel = null;
+
+
+	void Start () 
+	{
+		colorWheel = Instantiate(colorWheelPrefab);
+		colorWheel.Deactivate();		
+	}
+	
+	void Update () 
+	{
+		if(IsDoingColoring)
+		{
+			if(alreadySetupBeforeColoring == false)
+				SetStateBeforeColoring();
+			UpdateWhenColoring();
+		}
+		else
+		{
+
+		}
+	}
+
+    private void SetStateBeforeColoring()
+    {
+		alreadySetupBeforeColoring = true;
+		//coloringTimer.StartCountdown(currentColorable.BaseTimer * CurrentDifficultyModifier);
+		coloringTimer.StartCountdown(5f);
+		colorWheel.Activate();
+    }
+
+    private void UpdateWhenColoring()
+    {
+		coloringTimer.Update();
+		if(coloringTimer.IsNoTimeLeft)
+		{
+			SetStateAfterColoring();
+		}
+    }
+
+    private void SetStateAfterColoring()
+    {
+		IsDoingColoring = false;
+		alreadySetupBeforeColoring = false;
+		colorWheel.Deactivate();
+    }
+}
---
+++ Assets/TimerBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBar : MonoBehaviour 
+{
+	public Timer ColoringTimer = null;
+	private Image bar = null;
+    private Vector3 originalScale;
+
+    void Start () 
+	{
+		bar = GetComponent<Image>();
+		originalScale = bar.gameObject.transform.localScale;
+	}
+	
+	void Update () 
+	{
+		if(ColoringTimer.IsCountingDown)
+		{
+			float scale = ColoringTimer.PercentLeft;
+			bar.gameObject.transform.localScale = new Vector3(scale * originalScale.x, originalScale.y, originalScale.z); 
+		}
+		else
+		{
+			bar.gameObject.transform.localScale = 0f * originalScale;
+		}
+	}
+}
