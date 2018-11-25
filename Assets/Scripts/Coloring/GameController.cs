50f60e42fc0aa116bc5ecb070e7acf62fe52f81e
Jakub <jakub.svoboda8495@gmail.com>
2018-11-25 10:34:23 +0100

    Added timer from colorable

--- Assets/Scripts/Coloring/GameController.cs
+++ Assets/Scripts/Coloring/GameController.cs
@@ -163,7 +163,7 @@ public class GameController : MonoBehaviour
     private void SetStateBeforeColoring()
     {
         alreadySetupBeforeColoring = true;
-        //coloringTimer.StartCountdown(currentColorable.BaseTimer * CurrentDifficultyModifier);
+        timer.StartCountdown(currentMonsterHolder.MonsterOutlined.GetComponent<ColorableInstance>().Colorable.BaseTimer * currentDungeon.Difficulty);
         colorWheel.Activate(currentlySelectedSection.transform.position);
         List<Color> palette = GetRandomColorSet();
         colorWheel.InitializeColorPallette(palette);
